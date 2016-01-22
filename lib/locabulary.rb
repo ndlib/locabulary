16123b40805bd438fdc118f55ee8a841d635c660
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-01-22 16:00:03 -0500

    Moving predicate name up in the data structure
    Instead of duplicating the predicate name create a container for the
    information.

--- lib/locabulary.rb
+++ lib/locabulary.rb
@@ -15,7 +15,7 @@ module Locabulary
 
     attr_reader(*ATTRIBUTE_NAMES)
 
-    def initialize(attributes = {})
+    def initialize(attributes={})
       ATTRIBUTE_NAMES.each do |key|
         value = attributes.fetch(key) { attributes.fetch(key.to_s, nil) }
         send("#{key}=", value)
@@ -53,16 +53,17 @@ module Locabulary
     active_cache[predicate_name] ||= begin
       filename = filename_for_predicate_name(predicate_name: predicate_name)
       json = JSON.parse(File.read(filename))
+      predicate_name = json.fetch('predicate_name')
       json.fetch('values').each_with_object([]) do |item_values, mem|
         activated_on = Date.parse(item_values.fetch('activated_on'))
         next unless activated_on < as_of
         deactivated_on_value = item_values.fetch('deactivated_on', nil)
         if deactivated_on_value.nil?
-          mem << Item.new(item_values)
+          mem << Item.new(item_values.merge('predicate_name' => predicate_name))
         else
           deactivated_on = Date.parse(deactivated_on_value)
           next unless deactivated_on >= as_of
-          mem << Item.new(item_values)
+          mem << Item.new(item_values.merge('predicate_name' => predicate_name))
         end
         mem
       end.sort
--- lib/locabulary/schema.rb
+++ lib/locabulary/schema.rb
@@ -3,11 +3,11 @@ require 'dry/validation/schema'
 
 module Locabulary
   class Schema < Dry::Validation::Schema
+    key(:predicate_name) { |predicate_name| predicate_name.format?(/\A[a-z_]+\Z/) & predicate_name.filled? }
     key(:values) do |values|
       values.array? do
         values.each do |value|
           value.hash? do
-            value.key(:predicate_name) { |predicate_name| predicate_name.format?(/\A[a-z_]+\Z/) & predicate_name.filled? }
             value.key(:term_label) { |term_label| term_label.filled? }
             value.optional(:term_uri) { |term_label| term_label.none? | term_label.str?  }
             value.optional(:default_presentation_sequence) do |default_presentation_sequence|
