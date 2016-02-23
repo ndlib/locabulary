de3340cee517870acf8c546b41f768afeb7a9208
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-02-23 15:05:30 -0500

    Expanding comparable test to more scenarios

--- spec/lib/locabulary_spec.rb
+++ spec/lib/locabulary_spec.rb
@@ -76,10 +76,15 @@ RSpec.describe Locabulary do
 end
 
 RSpec.describe Locabulary::Item do
-  def test_sort_with_nil_and_integer
-    item_1 = Locabulary::Item.new(default_presentation_sequence: nil, label: 'Hello')
-    item_2 = Locabulary::Item.new(default_presentation_sequence: 2, label: 'World')
+  context 'Comparable' do
+    it 'sorts with nil and integers for presentation sequence' do
+      item_1 = Locabulary::Item.new(default_presentation_sequence: nil, term_label: 'Hello')
+      item_2 = Locabulary::Item.new(default_presentation_sequence: 2, term_label: 'World')
+      item_3 = Locabulary::Item.new(default_presentation_sequence: nil, term_label: 'Help')
+      item_4 = Locabulary::Item.new(default_presentation_sequence: 1, term_label: 'Bob')
+      item_5 = Locabulary::Item.new(default_presentation_sequence: 2, term_label: 'Apple')
 
-    assert_equal([item_1, item_2].sort, [item_2, item_1])
+      expect([item_1, item_2, item_3, item_4, item_5].sort).to eq([item_4, item_5, item_2, item_1, item_3])
+    end
   end
 end
