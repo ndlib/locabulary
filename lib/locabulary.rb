24909457f7bc6e7c8f0b3d0e94010c7c8b4f2a7d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-02-23 14:18:15 -0500

    Rearranging method order for cohesion
    The Comparable methods are grouped together for presentation purposes.

--- lib/locabulary.rb
+++ lib/locabulary.rb
@@ -10,11 +10,6 @@ module Locabulary
   end
 
   class Item
-    include Comparable
-    ATTRIBUTE_NAMES = [:predicate_name, :term_label, :term_uri, :deposit_label, :description, :grouping, :affiliation, :default_presentation_sequence, :activated_on, :deactivated_on].freeze
-
-    attr_reader(*ATTRIBUTE_NAMES)
-
     def initialize(attributes={})
       ATTRIBUTE_NAMES.each do |key|
         value = attributes.fetch(key) { attributes.fetch(key.to_s, nil) }
@@ -22,20 +17,29 @@ module Locabulary
       end
     end
 
-    SORT_SEQUENCE_FOR_NIL = 100_000_000
+    ATTRIBUTE_NAMES = [:predicate_name, :term_label, :term_uri, :deposit_label, :description, :grouping, :affiliation, :default_presentation_sequence, :activated_on, :deactivated_on].freeze
+
+    attr_reader(*ATTRIBUTE_NAMES)
+
+    private
+
+    attr_writer(*ATTRIBUTE_NAMES)
+
+    public
+
+    include Comparable
+
     def <=>(other)
       value = presentation_sequence <=> other.presentation_sequence
       return value unless value == 0
       term_label <=> other.term_label
     end
 
+    SORT_SEQUENCE_FOR_NIL = 100_000_000
+    private_constant :SORT_SEQUENCE_FOR_NIL
     def presentation_sequence
       default_presentation_sequence || SORT_SEQUENCE_FOR_NIL
     end
-
-    private
-
-    attr_writer(*ATTRIBUTE_NAMES)
   end
 
   module_function
