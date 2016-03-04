c646e4a91e54252772d85235bc1822c639d80505
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-03-04 10:10:17 -0500

    Improving documentation of Locabulary::Item
    There is some confusing concerning locabulary items and the intention
    of each of the attributes. This is my attempt to provide some clarify.
    
    Attention: @banurekha, @danhorst

--- lib/locabulary/item.rb
+++ lib/locabulary/item.rb
@@ -1,6 +1,42 @@
 module Locabulary
-  # A singular item in the controlled vocubulary
+  # A singular item in the controlled vocubulary.
+  # @see https://en.wikipedia.org/wiki/Resource_Description_Framework
   class Item
+    # [String] the trait for a given subject that we are describing by way of the term_label/term_uri
+    attr_reader :predicate_name
+
+    # [String] the human friendly version of the meaning for this given trait
+    # @note For the time being, please regard the term_label as immutable; If you need a modification, deactivate this one and activate a
+    #    new one
+    attr_reader :term_label
+
+    # [String] the machine friendly version of the meaning for this given trait
+    # @note For the time being, please regard the term_uri as immutable; If you need a modification, deactivate this one and activate a new
+    #    one
+    attr_reader :term_uri
+
+    # [String] a side-car of more exhaustive information related to this particular term
+    attr_reader :description
+
+    # [Date] When was this particular item activated
+    attr_reader :activated_on
+
+    # [Date] When was this particular item deactivated
+    attr_reader :deactivated_on
+
+    # [Integer, nil] What is the order in which
+    # @see Locabulary::Item#presentation_sequence for details on how this is calculated
+    attr_reader :default_presentation_sequence
+
+    # @deprecated
+    attr_reader :deposit_label
+
+    # @deprecated
+    attr_reader :grouping
+
+    # @deprecated
+    attr_reader :affiliation
+
     def initialize(attributes = {})
       ATTRIBUTE_NAMES.each do |key|
         value = attributes.fetch(key) { attributes.fetch(key.to_s, nil) }
@@ -21,8 +57,6 @@ module Locabulary
     end
     alias as_json to_h
 
-    attr_reader(*ATTRIBUTE_NAMES)
-
     private
 
     attr_writer(*ATTRIBUTE_NAMES)
@@ -31,6 +65,7 @@ module Locabulary
 
     include Comparable
 
+    # @todo Only objects with like predicate_name's should be compared
     def <=>(other)
       value = presentation_sequence <=> other.presentation_sequence
       return value unless value == 0
