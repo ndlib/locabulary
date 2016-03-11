f5df5e9b7091a6420a63bea5127461f42a5fcca9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-03-10 20:24:47 -0500

    Adding a method to tease out hierarchical_root

--- lib/locabulary/items/administrative_unit.rb
+++ lib/locabulary/items/administrative_unit.rb
@@ -5,11 +5,41 @@ module Locabulary
     #
     # @see ./data/administrative_units.json
     class AdministrativeUnit < Locabulary::Items::Base
+      # @note I'm assuming that I have but one top level node
+      def self.hierarchical_root(items = Locabulary.active_items_for(predicate_name: 'administrative_units'))
+        return @hierarchical_root if @hierarchical_root
+        graph = {}
+        items.each do |item|
+          graph[item.term_label] = item
+        end
+        top_slugs = []
+        items.each do |item|
+          parent_slugs = item.slugs
+          parent_slugs.pop
+          top_slugs << parent_slugs[0]
+          graph[parent_slugs.join(HIERARCHY_SEPARATOR)].children << item unless parent_slugs.empty?
+        end
+        # fail if top_slugss are more than one
+        @hierarchical_root = graph.fetch(top_slugs.first)
+      end
+
       configure do |config|
         config.attribute_names = [
           :predicate_name, :term_label, :term_uri, :description, :grouping, :affiliation, :default_presentation_sequence,
           :activated_on, :deactivated_on
         ]
+      end
+
+      def initialize(*args)
+        super
+        @children = []
+      end
+
+      attr_reader :children
+
+      HIERARCHY_SEPARATOR = '::'.freeze
+      def slugs
+        term_label.split(HIERARCHY_SEPARATOR)
       end
     end
   end
--- spec/lib/locabulary/items/administrative_unit_spec.rb
+++ spec/lib/locabulary/items/administrative_unit_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe Locabulary::Items::AdministrativeUnit do
   its(:attribute_names) { should include(:default_presentation_sequence) }
   its(:attribute_names) { should include(:activated_on) }
   its(:attribute_names) { should include(:deactivated_on) }
+
+  context '.hierarchical_root' do
+    it 'works if the hierarchy does not have skipped nodes' do
+      item1 = Locabulary::Items::AdministrativeUnit.new(term_label: 'Universe::Non-Galactic')
+      item2 = Locabulary::Items::AdministrativeUnit.new(term_label: 'Universe::Galaxy::Planet')
+      item3 = Locabulary::Items::AdministrativeUnit.new(term_label: 'Universe')
+      item4 = Locabulary::Items::AdministrativeUnit.new(term_label: 'Universe::Galaxy')
+      item5 = Locabulary::Items::AdministrativeUnit.new(term_label: 'Universe::Galaxy::Ketchup')
+      root = described_class.hierarchical_root([item1, item2, item3, item4, item5])
+
+      expect(root).to eq(item3)
+      expect(root.children.size).to eq(2)
+      expect(root.children).to eq([item1, item4])
+      expect(item4.children).to eq([item2, item5])
+      expect(item1.children).to eq([])
+    end
+
+    xit 'fails if we have empty spaces between our nodes'
+    xit 'fails if we have more than one root node'
+  end
 end
