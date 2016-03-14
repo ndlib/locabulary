d97a7e0484967b8903bff6c71b5351330787dfa9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-03-14 12:08:46 -0400

    Adding classification
    This is an attribute that showed up in the spreadsheet and needs to be
    accounted for.

--- lib/locabulary/items/administrative_unit.rb
+++ lib/locabulary/items/administrative_unit.rb
@@ -9,7 +9,7 @@ module Locabulary
     class AdministrativeUnit < Locabulary::Items::Base
       configure do |config|
         config.attribute_names = [
-          :predicate_name, :term_label, :term_uri, :description, :grouping, :affiliation, :default_presentation_sequence,
+          :predicate_name, :term_label, :term_uri, :description, :grouping, :classification, :affiliation, :default_presentation_sequence,
           :homepage, :activated_on, :deactivated_on
         ]
       end
@@ -17,14 +17,13 @@ module Locabulary
       # [String] What is the URL of the homepage. Please note the term_uri is reserved for something that is more resolvable by machines.
       #   And while the homepage may look resolvable, it is not as meaningful for longterm preservation.
       attr_reader :homepage
+      attr_reader :classification
       attr_reader :grouping
       attr_reader :affiliation
 
       private
 
-      attr_writer :homepage
-      attr_writer :grouping
-      attr_writer :affiliation
+      attr_writer :homepage, :classification, :grouping, :affiliation
 
       public
 
--- spec/lib/locabulary/items/administrative_unit_spec.rb
+++ spec/lib/locabulary/items/administrative_unit_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Locabulary::Items::AdministrativeUnit do
   its(:attribute_names) { should include(:term_uri) }
   its(:attribute_names) { should include(:description) }
   its(:attribute_names) { should include(:grouping) }
+  its(:attribute_names) { should include(:classification) }
   its(:attribute_names) { should include(:affiliation) }
   its(:attribute_names) { should include(:default_presentation_sequence) }
   its(:attribute_names) { should include(:activated_on) }
