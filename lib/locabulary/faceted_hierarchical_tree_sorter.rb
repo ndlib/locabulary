3b0c23379de8a7eb7cf6c6fb0acf6b9ee543facf
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-08-01 13:18:02 -0400

    Parameterizing the FacetedHierarchicalTreeSorter
    Provide a more keyword args style initializer. Also instead of a hard-
    coded predicate_name, add that as a parameter.

--- lib/locabulary/faceted_hierarchical_tree_sorter.rb
+++ lib/locabulary/faceted_hierarchical_tree_sorter.rb
@@ -1,11 +1,15 @@
 module Locabulary
   # Responsible for sorting a hierarchical facet tree.
   class FacetedHierarchicalTreeSorter
-    def initialize(tree = {})
-      @tree = tree
+    # @param options [Hash]
+    # @option tree [Hash]
+    # @option predicate_name [String]
+    def initialize(options = {})
+      @tree = options.fetch(:tree)
+      @predicate_name = options.fetch(:predicate_name)
     end
 
-    # This method return sorted locabulary hierarchical tree for given set of administrative_unit items
+    # This method return sorted locabulary hierarchical tree for given set of items for the given predicate_name
     def call
       nodes = []
       tree.each_pair do |key, sub_facets|
@@ -32,7 +36,7 @@ module Locabulary
     end
 
     def new_node(solr_facet_struct)
-      items = Locabulary.active_items_for(predicate_name: "administrative_units")
+      items = Locabulary.active_items_for(predicate_name: predicate_name)
       term_label = solr_facet_struct.qvalue.gsub(/(?<!:):(?!:)/, "::")
       items.each do |administrative_unit|
         if administrative_unit.term_label == term_label
