812ad6281d1de41903156e79fcf2a7aada6a603c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-08-05 12:21:02 -0400

    Extracting method
    Given that I need the specific class, and not just the builder method,
    I'm pulling apart the two methods.

--- lib/locabulary/item.rb
+++ lib/locabulary/item.rb
@@ -15,11 +15,12 @@ module Locabulary
     # @see Locabulary::Items
     def self.build(attributes = {})
       predicate_name = attributes.fetch(:predicate_name) { attributes.fetch('predicate_name') }
-      builder_for(predicate_name: predicate_name).call(attributes)
+      class_to_instantiate(predicate_name: predicate_name).new(attributes)
     end
 
     # @api public
     # @since 0.2.1
+    # @deprecated 0.6.0 Prefer instead class_to_instantiate
     #
     # Responsible for finding the appropriate Factory method for building a Locabulary::Item
     #
@@ -27,14 +28,25 @@ module Locabulary
     # @option predicate_name [String] Used for lookup of the correct Locabulary::Item type
     # @return [#call] A builder method (`.new` for the given constant)
     def self.builder_for(options = {})
+      class_to_instantiate(options).method(:new)
+    end
+
+    # @api public
+    # @since 0.5.1
+    #
+    # Responsible for finding the appropriate class that will be instantiated
+    #
+    # @param options [Hash]
+    # @option options [String] :predicate_name Used for lookup of the correct Locabulary::Item type
+    # @return [Locabulary::Items::Base]
+    def self.class_to_instantiate(options = {})
       predicate_name = options.fetch(:predicate_name)
       possible_class_name_for_predicate_name = predicate_name.singularize.classify
-      klass = begin
+      begin
         Items.const_get(possible_class_name_for_predicate_name)
       rescue NameError
         Items::Base
       end
-      klass.method(:new)
     end
   end
 end
--- spec/lib/locabulary/item_spec.rb
+++ spec/lib/locabulary/item_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'locabulary/item'
 
 RSpec.describe Locabulary::Item do
-  context '#build' do
+  context '.build' do
     it "finds a defined subclass" do
       expect(described_class.build(predicate_name: 'administrative_units')).to(
         be_a(Locabulary::Items::AdministrativeUnit)
@@ -13,7 +13,7 @@ RSpec.describe Locabulary::Item do
       expect(described_class.build(predicate_name: 'chicken')).to(be_a(Locabulary::Items::Base))
     end
   end
-  context '#builder_for' do
+  context '.builder_for' do
     it "finds a defined subclass" do
       expect(described_class.builder_for(predicate_name: 'administrative_units').call).to(
         be_a(Locabulary::Items::AdministrativeUnit)
@@ -22,6 +22,18 @@ RSpec.describe Locabulary::Item do
 
     it "defaults to the Items::Base" do
       expect(described_class.builder_for(predicate_name: 'chicken').call).to(be_a(Locabulary::Items::Base))
+    end
+  end
+
+  context '.class_to_instantiate' do
+    it "finds a defined subclass" do
+      expect(described_class.class_to_instantiate(predicate_name: 'administrative_units')).to(
+        eq(Locabulary::Items::AdministrativeUnit)
+      )
+    end
+
+    it "defaults to the Items::Base" do
+      expect(described_class.class_to_instantiate(predicate_name: 'chicken')).to(eq(Locabulary::Items::Base))
     end
   end
 end
